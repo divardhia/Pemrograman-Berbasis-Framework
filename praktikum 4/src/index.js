7c23b9845e25818cc8dc2afe366f885b06594f9f
divardhia <divardhiass@gmail.com>
2022-03-18 20:36:22 +0700

    praktikum modul 4

--- praktikum 4/src/index.js
+++ praktikum 4/src/index.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import BlogPost from './container/BlogPost/BlogPost';
+import MahasiswaAPI from './container/Mahasiswa/MahasiswaAPI';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(<BlogPost />, document.getElementById('content'));
 
+// ReactDOM.render(<BlogPost />, document.getElementById('content'));
+ReactDOM.render(<MahasiswaAPI />, document.getElementById('content'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
