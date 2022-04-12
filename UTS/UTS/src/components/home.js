eac6b4cca8a6f7931c5e6cc9b40ffc30672e5be1
divardhia <divardhiass@gmail.com>
2022-04-12 12:21:23 +0700

    UTS

---
+++ UTS/UTS/src/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+                <p>Konsep bisnis Kami menawarkan barang elektronik, fashion, dan furniture 
+                    dengan kualitas dan gambar yang dapat dilihat pada menu category dengan melakukan login terlebih dahulu. 
+                    untuk menghubungi kami dapat mengunjungi menu contact</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ UTS/UTS/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+import Main from './components/main';
+
+
+// ReactDOM.render(<AuthExample />, document.getElementById('root'));
+// ReactDOM.render(<ParamsExample />, document.getElementById('root'));
+// ReactDOM.render(<NestingExample />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
